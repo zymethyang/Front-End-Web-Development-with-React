e79f26d750f44e6c1a7e7a881b1ccd0e383283a5
Tri Nguyen <zymethyang@gmail.com>
2018-07-28 21:36:29 +0700

    React Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import { BrowserRouter } from 'react-router-dom';
 import Menu from './components/MenuComponent';
 import Main from './components/MainComponent';
 import { DISHES } from './shared/dishes';
@@ -14,9 +15,11 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="App">
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,9 @@ import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
 class Main extends Component {
 
     constructor(props) {
@@ -20,17 +23,18 @@ class Main extends Component {
     }
 
     render() {
+        const HomePage = () => {
+            return (
+                <Home
+                />
+            );
+        }
         return (
-            <div>
-                <Header />
-                <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-                {
-                    this.state.selectedDish ?
-                        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-                        : <div></div>
-                }
-                <Footer />
-            </div>
+            <Switch>
+                <Route path='/home' component={HomePage} />
+                <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                <Redirect to="/home" />
+            </Switch>
         );
     }
 }
