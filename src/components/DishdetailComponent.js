f5605ffb7ec1ed444d5a2b0433a70f9af79d55b6
Tri Nguyen <zymethyang@gmail.com>
2018-07-29 18:37:07 +0700

    Fetch from Server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -6,6 +6,7 @@ import {
 import { Link } from 'react-router-dom';
 import { Control, Errors, Form, actions } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 class CommentForm extends Component {
     constructor(props) {
@@ -147,7 +148,7 @@ const RenderDish = ({ dish }) => {
     if (dish != null)
         return (
             <Card>
-                <CardImg top src={dish.image} alt={dish.name} />
+                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                 <CardBody>
                     <CardTitle>{dish.name}</CardTitle>
                     <CardText>{dish.description}</CardText>
